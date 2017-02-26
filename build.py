c95c867d7293f206437a38387e02e24f8f6017c2
Minh930906 <minh930906@gmail.comgit config --global user.name Minh930906git config --global user.email minh930906@gmail.com>
2017-02-26 22:23:36 +0100

    testing fixing

--- build.py
+++ build.py
@@ -2,4 +2,5 @@ from connectdatabase import ConnectDatabase
 from models import Story
 
 ConnectDatabase.db.connect()
+ConnectDatabase.db.drop_tables([Story], safe=True)
 ConnectDatabase.db.create_tables([Story], safe=True)
--- sprint.py
+++ sprint.py
@@ -14,18 +14,21 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-@app.route('/story')
-def story():
-    return render_template('form.html')
+
 
 @app.route('/')
-def index():
-    return redirect(url_for('list'))
-
-@app.route('/and/list')
+@app.route('/list',methods=['GET'])
 def list():
     stories = Story.select().order_by(Story.id.asc())
     return render_template('list.html',stories=stories)
+
+@app.route('/story/<id>', methods=['GET','POST'])
+def story(id):
+    if Story.select().where(Story.id == id).exists():
+        user_story = Story.get(Story.id == id)
+    else:
+        user_story=None
+    return render_template("form.html",user_story=user_story)
 
 @app.route('/delete/<id>')
 def delete(id):
@@ -33,7 +36,8 @@ def delete(id):
     row.delete_instance()
     return redirect(url_for('list'))
 
-@app.route('/add', methods=['POST'])
+
+@app.route('/add', methods=['GET','POST'])
 def add_stories():
     storytitle=request.form['storytitle']
     userstory=request.form['userstory']
@@ -46,6 +50,25 @@ def add_stories():
     new_stories = Story.create(storytitle=storytitle,userstory=userstory,criteria=criteria,businessvalue=businessvalue,estimation=estimation,status=status)
     new_stories.save()
     flash('New entry was successfully posted')
+    return redirect(url_for('list'))
+
+
+@app.route('/update_story', methods=['POST'])
+def update_story():
+    id=request.form['id']
+    storytitle=request.form['storytitle']
+    userstory=request.form['userstory']
+    criteria = request.form['criteria']
+    businessvalue = request.form['businessvalue']
+    estimation = request.form['estimation']
+    status = request.form['status']
+    user_story = Story.update(storytitle=storytitle,
+                              userstory=userstory,
+                              criteria=criteria,
+                              businessvalue=businessvalue,
+                              estimation=estimation,
+                              status=status).where(Story.id == id)
+    user_story.execute()
     return redirect(url_for('list'))
 
 if __name__ == "__main__":
